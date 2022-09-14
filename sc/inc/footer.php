008e1f1f09bcf3fe6085763593468646f55c79ca
Bheru-Gadri78 <kbssalesforce78@gmail.com>
2022-09-14 18:33:22 +0530

    SC

---
+++ sc/inc/footer.php
@@ -0,0 +1,35 @@
+    
+        <div class="footer">
+            <div class="no-gutters">
+                <div class="col-auto mx-auto">
+                    <div class="row no-gutters justify-content-center">
+                        <div class="col-auto">
+                            <a href="index.php" class="btn btn-link-default active">
+                                <i class="material-icons">home</i>
+                            </a>
+                        </div>
+                        <div class="col-auto">
+                            <a href="notification.html" class="btn btn-link-default">
+                                <i class="material-icons">info</i>
+                            </a>
+                        </div>
+                        <div class="col-auto">
+                            <a href="total-book.php" class="btn btn-default shadow centerbutton">
+                                <i class="material-icons">local_mall</i>
+                            </a>
+                        </div>
+                        <div class="col-auto">
+                            <a href="history.html" class="btn btn-link-default">
+                                <i class="material-icons">history</i>
+                            </a>
+                        </div>
+                        <div class="col-auto">
+                            <a href="profile.php" class="btn btn-link-default">
+                                <i class="material-icons">account_circle</i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ sc/sc.php
@@ -0,0 +1,106 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<!-- Mirrored from www.enableds.com/products/stickymobile2/component-20-working-hours.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 05 Dec 2019 13:20:51 GMT -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+<title>StickyMobile</title>
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900,900i|Source+Sans+Pro:300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="styles/framework.css">
+<link rel="stylesheet" type="text/css" href="fonts/css/fontawesome-all.min.css">
+
+<link rel="manifest" href="_manifest.json" data-pwa-version="set_by_pwa.html">
+<link rel="apple-touch-icon" sizes="180x180" href="app/icons/icon-192x192.png">
+ <link rel="stylesheet" href="vendor/materializeicon/material-icons.css">
+
+    <!-- Roboto fonts CSS -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&amp;display=swap" rel="stylesheet">
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap-4.3.1/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Swiper CSS -->
+    <link href="vendor/swiper/css/swiper.min.css" rel="stylesheet">
+
+    <!-- Chosen multiselect CSS -->
+    <link href="vendor/chosen_v1.8.7/chosen.min.css" rel="stylesheet">
+
+    <!-- nouislider CSS -->
+    <link href="vendor/nouislider/nouislider.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+<body >
+
+	
+
+
+    <div class="row no-gutters vh-100 loader-screen">
+        <div class="col align-self-center text-white text-center">
+            <img src="img/logo.png" alt="logo">
+            <h1><span class="font-weight-light">Go</span>Fruit</h1>
+            <div class="laoderhorizontal">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+    <div class="wrapper">
+        <div class="header">
+            <div class="row no-gutters">
+                <div class="col-auto">
+                    <a href="doctor.html" class="btn  btn-link text-dark"><i class="material-icons">navigate_before</i></a>
+                </div>
+                <div class="col text-center"><img src="img/logo-header.png" alt="" class="header-logo"></div>
+                <div class="col-auto">
+                    <a href="profile.html" class="btn  btn-link text-dark"><i class="material-icons">account_circle</i></a>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+<div id="page">
+
+
+
+
+
+<div class="divider divider-margins"></div>
+
+<div class="content content-boxed content-boxed-full round-medium shadow-huge">
+<div data-height="130" data-closed-message="Sorry! We're closed!" data-closed-message-under="We'll be back very soon!" data-opened-message="Welcome! We're open!" data-opened-message-under="Call us Now! Let's have a chat!" class="business-hours caption shadow-huge bottom-10">
+<div class="caption-center right-15 top-15 text-right">
+<a id="business-hours-call" href="tel:+1 234 567 890" class="button button-xs button-round-huge bg-white color-black">Call Now</a>
+<a id="business-hours-mail" href="page-contact.html" class="button button-xs button-round-huge bg-white color-black">Email Us</a>
+</div>
+<div class="caption-center left-15 text-left">
+<h1 class="color-white bolder"></h1>
+<p class="under-heading color-white opacity-90 bottom-0"></p>
+</div>
+<div class="caption-overlay show-business-opened bg-green1-dark"></div>
+<div class="caption-overlay show-business-closed bg-red2-dark"></div>
+</div>
+<div class="content bottom-0">
+
+
+<div class="working-hours day-monday"> <p>Monday</p> <p>09:00 AM</p> <p>05:00 PM</p></div>
+<div class="working-hours day-tuesday"> <p>Tuesday</p> <p>09:00 AM</p> <p>05:00 PM</p></div>
+<div class="working-hours day-wednesday"> <p>Wednesday</p> <p>09:00 AM</p> <p>05:00 PM</p></div>
+<div class="working-hours day-thursday"> <p>Thursday</p> <p>09:00 AM</p> <p>05:00 PM</p></div>
+<div class="working-hours day-friday"> <p>Friday</p> <p>09:00 AM</p> <p>05:00 PM</p></div>
+<div class="working-hours day-saturday"> <p>Saturday</p> <p>09:00 AM</p> <p>01:00 PM</p></div>
+<div class="working-hours day-sunday bottom-15"><p>Sunday</p> <p class="opacity-00">-</p> <p>We're Closed</p></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<script type="text/javascript" src="scripts/jquery.js"></script>
+<script type="text/javascript" src="scripts/plugins.js"></script>
+<script type="text/javascript" src="scripts/custom.js"></script>
+</body>
